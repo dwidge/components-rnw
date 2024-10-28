00a9a3a36bd734c0aa190d13927dc3e42216823b
dwidge <dwidge+gh@gmail.com>
2024-10-28 12:03:29 +0200

    Add iconColor to StyledButton

--- src/StyledButton.tsx
+++ src/StyledButton.tsx
@@ -3,19 +3,21 @@
 // https://www.boost.org/LICENSE_1_0.txt
 
 import React from "react";
-import { Button, ButtonProps } from "@rneui/themed";
+import { Button, ButtonProps, useTheme } from "@rneui/themed";
 import { StyledIcon } from "./IconButton.js";
 import { Ionicons } from "@expo/vector-icons";
 import { useAsync } from "@dwidge/hooks-react";
 import { notifyError } from "./notify.js";
 
 export const StyledButton = ({
-  icon,
   onPress,
   loading = undefined,
   loader: [onPressLoader, loading2, error] = useAsync(
-    onPress ? async () => onPress() : undefined
+    onPress ? async () => onPress() : undefined,
   ),
+  disabled = !onPressLoader,
+  icon,
+  iconColor = disabled ? useTheme().theme.colors.greyOutline : "white",
   ...props
 }: Omit<ButtonProps, "onPress" | "icon"> & {
   onPress?: () => unknown;
@@ -23,14 +25,15 @@ export const StyledButton = ({
   loader?: [
     f: ((...args: any[]) => Promise<any>) | undefined,
     busy: boolean,
-    error: Error | undefined
+    error: Error | undefined,
   ];
+  iconColor?: string;
 }) => (
   <Button
     loading={loading || loading2}
-    icon={icon && <StyledIcon icon={icon} />}
+    icon={icon && <StyledIcon icon={icon} color={iconColor} />}
     onPress={() => onPressLoader?.().catch((e) => notifyError(e))}
-    disabled={!onPressLoader}
+    disabled={disabled}
     {...props}
   />
 );
