4cd62a4267a31a5c7d8dc302a2c33e17bea2f29b
dwidge <dwidge+gh@gmail.com>
2025-01-22 10:28:00 +0200

    Add ContextError toString

--- src/ContextError.ts
+++ src/ContextError.ts
@@ -4,21 +4,37 @@
 
 export class ContextError extends Error {
   context: any;
-  constructor(message: string, options: { cause?: any; context?: any }) {
+
+  constructor(message: string, options: { cause?: any; context?: any } = {}) {
     super(message, options);
+    this.name = "ContextError";
     this.context = options.context;
   }
-}
 
-export const logContextError = (e: unknown) => {
-  if (e instanceof Error) {
-    console.log(
-      "logContextError1",
-      e.message,
-      e instanceof ContextError ? e.context : e,
-    );
-    if (e.cause) logContextError(e.cause);
-  } else {
-    console.log("logContextError2", e);
+  private indent = (str: string, indent: number): string => {
+    const indentation = " ".repeat(indent);
+    return str
+      .split("\n")
+      .map((line) => `${indentation}${line}`)
+      .join("\n");
+  };
+
+  override toString(): string {
+    let result = `${this.name}: ${this.message}`;
+
+    if (this.context) {
+      const contextString = JSON.stringify(this.context, null, 2);
+      result += `\n  Context:\n${this.indent(contextString, 4)}`;
+    }
+
+    if (this.cause) {
+      const causeString =
+        this.cause instanceof Error
+          ? this.cause.toString()
+          : String(this.cause);
+      result += `\n  Cause:\n${this.indent(causeString, 4)}`;
+    }
+
+    return result;
   }
-};
+}
