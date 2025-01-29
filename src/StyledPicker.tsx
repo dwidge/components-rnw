b2acd95886992d426531ee995033b57f15d49ae9
dwidge <dwidge+gh@gmail.com>
2025-01-29 08:11:15 +0200

    Add param StyledPicker style

--- src/StyledPicker.tsx
+++ src/StyledPicker.tsx
@@ -2,15 +2,16 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-import React from "react";
-import { Colors, Theme, useTheme } from "@rneui/themed";
 import { Picker } from "@react-native-picker/picker";
+import { Colors, Theme, useTheme } from "@rneui/themed";
+import { StyleProp, TextStyle } from "react-native";
 
 export const StyledPicker = <T,>({
   value,
   onChange,
   options,
   theme = useTheme().theme,
+  style,
 }: {
   value: T;
   onChange?: (v: (prev: T) => T) => unknown;
@@ -18,15 +19,19 @@ export const StyledPicker = <T,>({
   theme?: {
     colors: Colors;
   } & Theme;
+  style?: StyleProp<TextStyle>;
 }) => (
   <Picker
     selectedValue={value}
     onValueChange={(v) => onChange?.(() => v)}
-    style={{
-      fontSize: 16,
-      color: theme.colors.black,
-      backgroundColor: theme.colors.white,
-    }}
+    style={[
+      {
+        fontSize: 16,
+        color: theme.colors.black,
+        backgroundColor: theme.colors.white,
+      },
+      style,
+    ]}
   >
     {value == undefined || value === "" ? (
       <Picker.Item label="None" value={undefined} />
