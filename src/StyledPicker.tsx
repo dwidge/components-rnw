6ada9c5c649b789bcc19ef691da085f37df80209
dwidge <dwidge+gh@gmail.com>
2025-02-01 10:46:30 +0200

    Remove hard coded option None from StyledPicker

--- src/StyledPicker.tsx
+++ src/StyledPicker.tsx
@@ -5,6 +5,7 @@
 import { Picker } from "@react-native-picker/picker";
 import { Colors, Theme, useTheme } from "@rneui/themed";
 import { StyleProp, TextStyle } from "react-native";
+import { StyledLoader } from "./StyledLoader";
 
 export const StyledPicker = <T,>({
   value,
@@ -13,31 +14,34 @@ export const StyledPicker = <T,>({
   theme = useTheme().theme,
   style,
 }: {
-  value: T;
+  value?: T;
   onChange?: (v: (prev: T) => T) => unknown;
   options?: { label: string; value: T }[];
   theme?: {
     colors: Colors;
   } & Theme;
   style?: StyleProp<TextStyle>;
-}) => (
-  <Picker
-    selectedValue={value}
-    onValueChange={(v) => onChange?.(() => v)}
-    style={[
-      {
-        fontSize: 16,
-        color: theme.colors.black,
-        backgroundColor: theme.colors.white,
-      },
-      style,
-    ]}
-  >
-    {value == undefined || value === "" ? (
-      <Picker.Item label="None" value={undefined} />
-    ) : null}
-    {options?.map((option, index) => (
-      <Picker.Item key={index} label={option.label} value={option.value} />
-    ))}
-  </Picker>
-);
+}) =>
+  value === undefined ? (
+    <StyledLoader />
+  ) : (
+    <Picker
+      selectedValue={value}
+      onValueChange={onChange ? (v) => onChange(() => v) : undefined}
+      style={[
+        {
+          fontSize: 16,
+          color: theme.colors.black,
+          backgroundColor: theme.colors.white,
+        },
+        style,
+      ]}
+    >
+      {/* {value == undefined || value === "" ? (
+        <Picker.Item label="None" value={undefined} />
+      ) : null} */}
+      {options?.map((option, index) => (
+        <Picker.Item key={index} label={option.label} value={option.value} />
+      ))}
+    </Picker>
+  );
