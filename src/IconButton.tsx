692448964307cc27d3881dea569d9869f0502a81
dwidge <dwidge+gh@gmail.com>
2025-11-01 19:10:33 +0200

    feat(IconButton): Add error handler and loader to onPress

--- src/IconButton.tsx
+++ src/IconButton.tsx
@@ -1,11 +1,14 @@
+import { useAsync } from "@dwidge/hooks-react";
+import { sleep } from "@dwidge/utils-js";
 import { withTheme } from "@rneui/themed";
-import { PropsWithChildren, useMemo } from "react";
+import { PropsWithChildren, useContext, useMemo } from "react";
 import {
   ColorValue,
   StyleProp,
   TouchableOpacity,
   ViewStyle,
 } from "react-native";
+import { EventErrorHandlerContext } from "./EventErrorHandler";
 import { getIconComponent, IconGlyph } from "./IconName";
 import { StyledView } from "./StyledView";
 
@@ -29,25 +32,42 @@ export const IconButton = withTheme(
     children,
     ...props
   }: IconButtonProps) => {
+    const onError = useContext(EventErrorHandlerContext);
+
+    const [onPressLoader, loading2] = useAsync(
+      onPress
+        ? async () => {
+            await sleep(0);
+            try {
+              return await onPress();
+            } catch (e) {
+              onError(e);
+            }
+          }
+        : undefined,
+    );
+
     const IconComponent = useMemo(
       () => getIconComponent(name as string),
       [name],
     );
 
     return (
-      <TouchableOpacity onPress={onPress} style={style}>
+      <TouchableOpacity onPress={onPressLoader} style={style}>
         <StyledView
           flex
           center
           middle
           row
           sgap
-          style={[{ padding: 10, margin: -10, opacity: onPress ? 1 : 0.2 }]}
+          style={[
+            { padding: 10, margin: -10, opacity: onPressLoader ? 1 : 0.2 },
+          ]}
         >
           <IconComponent
             name={name as any}
             size={size}
-            color={onPress ? color : disabledColor}
+            color={onPressLoader ? color : disabledColor}
             style={{
               width: size,
               height: size,
